ea774806a7eef3d0f2f70f5334a6f4a89a6563b1
Stan-breaks <stanleymwendwa03@gmail.com>
2024-12-14 05:21:47 +0300

    day4 part1

--- day4/script1/main.zig
+++ day4/script1/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file = try std.fs.cwd().openFile("test_input.txt", .{});
+    const file = try std.fs.cwd().openFile("advent_input.txt", .{});
     defer file.close();
 
     const content = try file.readToEndAlloc(allocator, 1024 * 1024);
@@ -31,11 +31,44 @@ pub fn main() !void {
     var count: i64 = 0;
     for (0..arr.items.len) |i| {
         for (0..arr.items[i].len) |j| {
-            if (std.mem.startsWith(u8, arr.items[i][j..], "SAMX") or std.mem.startsWith(u8, arr.items[i][j..], "XMAS")) {
-                count += 1;
-            }
-            if (std.mem.startsWith(u8, arr.items[j][i..], "SAMX") or std.mem.startsWith(u8, arr.items[j][i..], "XMAS")) {
-                count += 1;
+            if (arr.items[i][j] == 'S' or arr.items[i][j] == 'X' or arr.items[j][i] == 'S' or arr.items[j][i] == 'X') {
+                if (std.mem.startsWith(u8, arr.items[i][j..], "SAMX") or std.mem.startsWith(u8, arr.items[i][j..], "XMAS")) {
+                    count += 1;
+                }
+                if (std.mem.startsWith(u8, arr.items[j][i..], "SAMX") or std.mem.startsWith(u8, arr.items[j][i..], "XMAS")) {
+                    count += 1;
+                }
+                const signedI: i64 = @intCast(i);
+                const signedJ: i64 = @intCast(j);
+
+                if (i + 4 <= arr.items.len and j + 4 <= arr.items[i].len) {
+                    var forwardDiagonal = std.ArrayList(u8).init(allocator);
+                    defer forwardDiagonal.deinit();
+                    var r = i;
+                    var c = j;
+                    while (r < i + 4 and c < j + 4) {
+                        try forwardDiagonal.append(arr.items[r][c]);
+                        r += 1;
+                        c += 1;
+                    }
+                    if (std.mem.startsWith(u8, forwardDiagonal.items, "SAMX") or std.mem.startsWith(u8, forwardDiagonal.items, "XMAS")) {
+                        count += 1;
+                    }
+                }
+                if (signedI - 4 >= 0 and signedJ - 4 >= 0) {
+                    var backDiagonal = std.ArrayList(u8).init(allocator);
+                    defer backDiagonal.deinit();
+                    var r = i;
+                    var c = j;
+                    while (r > i - 4 and c > j - 4) {
+                        try backDiagonal.append(arr.items[r][c]);
+                        r -= 1;
+                        c -= 1;
+                    }
+                    if (std.mem.startsWith(u8, backDiagonal.items, "SAMX") or std.mem.startsWith(u8, backDiagonal.items, "XMAS")) {
+                        count += 1;
+                    }
+                }
             }
         }
     }
