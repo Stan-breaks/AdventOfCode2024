50c3173d210732371897b2eaa19d973cdccb8a3e
Stan-breaks <stanleymwendwa03@gmail.com>
2024-12-18 17:55:27 +0300

    day6 part2

--- day6/script2/main.zig
+++ day6/script2/main.zig
@@ -76,7 +76,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file = try std.fs.cwd().openFile("test_input.txt", .{});
+    const file = try std.fs.cwd().openFile("advent_input.txt", .{});
     defer file.close();
 
     const content = try file.readToEndAlloc(allocator, 1024 * 1024);
@@ -115,18 +115,16 @@ pub fn main() !void {
     while (currentState.i > 0 and currentState.i < map.items.len - 1 and
         currentState.j > 0 and currentState.j < map.items[0].len - 1)
     {
-        if (previousStates.items.len > 1) {
-            map.items[currentState.i][currentState.j] = '#';
-            if (isGuardStuck(previousStates.items[previousStates.items.len - 2], map.items, allocator)) {
-                loops += 1;
-            }
-            map.items[currentState.i][currentState.j] = '.';
-        } else {
-            map.items[currentState.i][currentState.j] = '.';
-            currentState.i -= 1;
-        }
         try previousStates.append(currentState);
         moveGuard(&currentState, map.items);
+    }
+    for (1..previousStates.items.len - 1) |i| {
+        const testState = previousStates.items[i - 1];
+        map.items[previousStates.items[i].i][previousStates.items[i].j] = '#';
+        if (isGuardStuck(testState, map.items, allocator)) {
+            loops += 1;
+        }
+        map.items[previousStates.items[i].i][previousStates.items[i].j] = '.';
     }
     try stdout.print("{d}\n", .{loops});
 }
