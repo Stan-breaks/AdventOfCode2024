81b4c988b7e1f171875ba1e1294a0cb9dcff8ff6
Stan-breaks <stanleymwendwa03@gmail.com>
2024-12-23 11:42:30 +0300

    day7 part2
    remove repeated allocation in the combinations for speed

--- day7/script2/main.zig
+++ day7/script2/main.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
-fn tryAllcombination(numbers: []i64, target: i64, allocator: std.mem.Allocator) bool {
+fn tryAllcombination(numbers: []i64, target: i64) bool {
     if (numbers.len == 1) return target == numbers[0];
     if (numbers.len == 0) return false;
 
     const length: usize = numbers.len - 1;
     const combinations = std.math.pow(usize, 3, length);
     var i: usize = 0;
+    var buffer: [32]u8 = undefined;
     while (i < combinations) : (i += 1) {
         var total: i64 = numbers[0];
         var j: usize = 0;
@@ -14,17 +15,12 @@ fn tryAllcombination(numbers: []i64, target: i64, allocator: std.mem.Allocator) 
             const op = tempI % 3;
             tempI /= 3;
             switch (op) {
-                0 => {
-                    total += numbers[j + 1];
-                },
-                1 => {
-                    total *= numbers[j + 1];
-                },
+                0 => total += numbers[j + 1],
+                1 => total *= numbers[j + 1],
                 2 => {
-                    const str = std.fmt.allocPrint(allocator, "{d}{d}", .{ total, numbers[j + 1] }) catch {
+                    const str = std.fmt.bufPrint(&buffer, "{d}{d}", .{ total, numbers[j + 1] }) catch {
                         return false;
                     };
-                    defer allocator.free(str);
                     total = std.fmt.parseInt(i64, str, 10) catch {
                         return false;
                     };
@@ -33,7 +29,7 @@ fn tryAllcombination(numbers: []i64, target: i64, allocator: std.mem.Allocator) 
             }
         }
         if (total == target) {
-            return target;
+            return true;
         }
     }
     return false;
@@ -43,7 +39,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file = try std.fs.cwd().openFile("test_input.txt", .{});
+    const file = try std.fs.cwd().openFile("advent_input.txt", .{});
     defer file.close();
 
     const content = try file.readToEndAlloc(allocator, 1024 * 1024);
@@ -63,7 +59,7 @@ pub fn main() !void {
                 const num = try std.fmt.parseInt(i64, val, 10);
                 try numbers.append(num);
             }
-            if (tryAllcombination(numbers.items, target, allocator)) {
+            if (tryAllcombination(numbers.items, target)) {
                 sum += target;
             }
         }
