9598da0f38cccec8d9511857e27b6764fb89ce60
Stan-breaks <stanleymwendwa03@gmail.com>
2024-12-28 11:44:41 +0300

    day9 part1
    using an arr of arr to handle more than 9 ID

--- day9/script1/main.zig
+++ day9/script1/main.zig
@@ -13,25 +13,31 @@ pub fn main() !void {
 
     const stdout = std.io.getStdOut().writer();
 
-    var arr = std.ArrayList(u8).init(allocator);
-    defer arr.deinit();
+    var arr = std.ArrayList([]u8).init(allocator);
+    defer {
+        for (arr.items) |item| {
+            allocator.free(item);
+        }
+        arr.deinit();
+    }
 
-    var id: usize = 0;
+    var id: i32 = 0;
     for (0..content.len - 1) |i| {
         if (i % 2 == 0) {
-            const str: u8 = @intCast(id + 48);
-            const size: usize = @intCast(content[i] - 48);
+            const size: usize = @intCast(content[i] - '0');
             if (size > 0) {
                 for (size) |_| {
+                    const str = try std.fmt.allocPrint(allocator, "{}", .{id});
                     try arr.append(str);
                 }
                 id += 1;
             }
         } else {
-            const space: usize = @intCast(content[i] - 48);
+            const space: usize = @intCast(content[i] - '0');
             if (space > 0) {
                 for (space) |_| {
-                    try arr.append('.');
+                    const dot = try std.fmt.allocPrint(allocator, ".", .{});
+                    try arr.append(dot);
                 }
             }
         }
@@ -39,20 +45,22 @@ pub fn main() !void {
     var left: usize = 0;
     var right: usize = arr.items.len - 1;
     while (left < right) {
-        if (arr.items[left] == '.') {
-            arr.items[left] = arr.items[right];
-            arr.items[right] = '.';
+        if (std.mem.eql(u8, arr.items[left], ".")) {
+            const dot = arr.items[left];
+            const num = arr.items[right];
+            arr.items[left] = num;
+            arr.items[right] = dot;
             right -= 1;
         } else {
             left += 1;
         }
     }
 
-    var sum: i32 = 0;
+    var sum: i64 = 0;
     for (0..arr.items.len) |i| {
-        if (arr.items[i] != '.') {
-            const num: usize = @intCast(arr.items[i]);
-            sum += @intCast(i * (num - 48));
+        if (!std.mem.eql(u8, arr.items[i], ".")) {
+            const num = try std.fmt.parseInt(i64, arr.items[i], 10);
+            sum += (@as(i64, @intCast(i)) * num);
         } else {
             break;
         }
