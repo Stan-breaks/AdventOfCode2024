0f49473b90128de03d87155d0f905b1ea6faa34e
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-05 13:42:13 +0300

    day10 part1
    finding a tail for each zero

--- day10/script1/main.zig
+++ day10/script1/main.zig
@@ -1,4 +1,36 @@
 const std = @import("std");
+
+const Position = struct {
+    i: usize,
+    j: usize,
+    value: usize,
+};
+
+fn findTailHead(map: [][]u8, num: Position) i32 {
+    var current = Position{ .i = num.i, .j = num.j, .value = num.value };
+    while (current.i > -1 and current.i < map.len and current.j > -1 and current.j < map[current.i].len) {
+        if (current.value == 9) {
+            return 1;
+        }
+        if (current.i > 0 and map[current.i - 1][current.j] == current.value + 1) {
+            current.i -= 1;
+            current.value += 1;
+        } else if (current.j > 0 and map[current.i][current.j - 1] == current.value + 1) {
+            current.j -= 1;
+            current.value += 1;
+        } else if (current.i < map.len - 1 and map[current.i + 1][current.j] == current.value + 1) {
+            current.i += 1;
+            current.value += 1;
+        } else if (current.j < map[current.i].len - 1 and map[current.i][current.j + 1] == current.value + 1) {
+            current.j += 1;
+            current.value += 1;
+        } else {
+            return 0;
+        }
+    }
+    return 0;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -9,7 +41,6 @@ pub fn main() !void {
 
     const content = try file.readToEndAlloc(allocator, 1024 * 1024);
     defer allocator.free(content);
-
     const stdout = std.io.getStdOut().writer();
 
     var map = std.ArrayList([]u8).init(allocator);
@@ -25,11 +56,18 @@ pub fn main() !void {
         @memcpy(mutableLine, line);
         try map.append(mutableLine);
     }
+    var result: i32 = 0;
     for (0..map.items.len) |i| {
         for (0..map.items[i].len) |j| {
             if (map.items[i][j] == '0') {
-                try stdout.print("i:{d} j:{d}\n", .{ i, j });
+                const num = Position{
+                    .i = i,
+                    .j = j,
+                    .value = 0,
+                };
+                result += findTailHead(map.items, num);
             }
         }
     }
+    try stdout.print("tailheads: {d}", .{result});
 }
