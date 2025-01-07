70bd744f00ef7e1486c0b7db6a8296a3c366d9f8
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-07 20:18:25 +0300

    day11 part1
    main logic

--- day11/script1/main.zig
+++ day11/script1/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
+fn blink(arr: []i32, allocator: std.mem.Allocator) std.ArrayList(i32) {
+    var result = std.ArrayList(i32).init(allocator);
+    defer result.deinit();
+    for (arr) |item| {
+        const str = std.fmt.allocPrint(allocator, "{d}", .{item}) catch return result;
+        defer allocator.free(str);
+        if (item == 0) {
+            result.append(1) catch return result;
+        } else if (str.len % 2 == 0) {
+            const num1 = std.fmt.parseInt(i32, str[0 .. str.len / 2], 10) catch return result;
+            const num2 = std.fmt.parseInt(i32, str[str.len / 2 ..], 10) catch return result;
+            result.append(num1) catch return result;
+            result.append(num2) catch return result;
+        } else {
+            result.append(item * 2024) catch return result;
+        }
+    }
+    return result;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -16,8 +36,15 @@ pub fn main() !void {
     var arr = std.ArrayList(i32).init(allocator);
     defer arr.deinit();
 
-    var numberTokenizer = std.mem.tokenize(u8, content," ");
-    while(numberTokenizer.next())|num|{
-
+    var numberTokenizer = std.mem.tokenize(u8, content, "   ");
+    while (numberTokenizer.next()) |val| {
+        const num = try std.fmt.parseInt(i32, val, 10);
+        try arr.append(num);
     }
+
+    for (0..6) |_| {
+        arr = blink(arr.items, allocator);
+    }
+
+    try stdout.print("{d}\n", .{arr.items.len});
 }
