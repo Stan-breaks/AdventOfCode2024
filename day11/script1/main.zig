1bf701e6519f0048cffa65d6711df4370492dcfa
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-07 19:23:06 +0300

    day11 part1
    init

---
+++ day11/script1/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file = try std.fs.cwd().openFile("test_input.txt", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(allocator, 1024 * 1024);
+    defer allocator.free(content);
+}
