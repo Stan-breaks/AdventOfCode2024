5effd187910c988227acb2d407292b7b2438a23d
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-07 20:40:05 +0300

    day11 part1
    done

--- day11/script1/main.zig
+++ day11/script1/main.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
 
-fn blink(arr: []i32, allocator: std.mem.Allocator) std.ArrayList(i32) {
-    var result = std.ArrayList(i32).init(allocator);
-    defer result.deinit();
+fn blink(arr: []i64, allocator: std.mem.Allocator) std.ArrayList(i64) {
+    var result = std.ArrayList(i64).init(allocator);
     for (arr) |item| {
         const str = std.fmt.allocPrint(allocator, "{d}", .{item}) catch return result;
         defer allocator.free(str);
         if (item == 0) {
             result.append(1) catch return result;
         } else if (str.len % 2 == 0) {
-            const num1 = std.fmt.parseInt(i32, str[0 .. str.len / 2], 10) catch return result;
-            const num2 = std.fmt.parseInt(i32, str[str.len / 2 ..], 10) catch return result;
+            const num1 = std.fmt.parseInt(i64, str[0 .. str.len / 2], 10) catch return result;
+            const num2 = std.fmt.parseInt(i64, str[str.len / 2 ..], 10) catch return result;
             result.append(num1) catch return result;
             result.append(num2) catch return result;
         } else {
@@ -25,7 +24,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file = try std.fs.cwd().openFile("test_input.txt", .{});
+    const file = try std.fs.cwd().openFile("advent_input.txt", .{});
     defer file.close();
 
     const content = try file.readToEndAlloc(allocator, 1024 * 1024);
@@ -33,17 +32,19 @@ pub fn main() !void {
 
     const stdout = std.io.getStdOut().writer();
 
-    var arr = std.ArrayList(i32).init(allocator);
+    var arr = std.ArrayList(i64).init(allocator);
     defer arr.deinit();
 
-    var numberTokenizer = std.mem.tokenize(u8, content, "   ");
+    var numberTokenizer = std.mem.tokenize(u8, content, "    ");
     while (numberTokenizer.next()) |val| {
-        const num = try std.fmt.parseInt(i32, val, 10);
+        const num = try std.fmt.parseInt(i64, std.mem.trim(u8, val, "\n"), 10);
         try arr.append(num);
     }
 
-    for (0..6) |_| {
-        arr = blink(arr.items, allocator);
+    for (0..25) |_| {
+        const new_arr = blink(arr.items, allocator);
+        arr.deinit();
+        arr = new_arr;
     }
 
     try stdout.print("{d}\n", .{arr.items.len});
